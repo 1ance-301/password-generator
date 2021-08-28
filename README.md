0ed77771b949bf2e707eb7322204e4213b5ad0fe
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-08-28 14:24:22 -0400

    prompt questions

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ A website that generates a random password for its user.
 * JavaScript
 
 ## Contribution
-Edited with ❤️ by Lance Bontrager
-Starter Code Cloned From https://github.com/coding-boot-camp/friendly-parakeet.git
+- Edited with ❤️ by Lance Bontrager
+- Starter Code Cloned From https://github.com/coding-boot-camp/friendly-parakeet.git
 
 ## Website Screenshot
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
-// Assignment code here
+// generates a password
+var generatePassword = function() {
+  // ask for the password length
+  var length = window.prompt("How many characters do you want include in the password?")
 
+  if (length >= 8 && length <= 128) {
+    console.log(length);
+  } else {
+    window.alert("The password needs to be between 8 and 128 characters.");
+
+    return generatePassword();
+  }
+  
+  // asks for character types included in the password
+  var lowercase = window.confirm("Do you want to include lowercase?");
+  if (lowercase === true) {
+    console.log(lowercase);
+  } else {
+    console.log(lowercase)
+  }
+  var uppercase = window.confirm("Do you want to include uppercase?");
+  var numbers = window.confirm("Do you want to include numbers?");
+  var characters = window.confirm("Do you want to include special characters?");
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
